49d934065d3dc94f757693e6be64e878e6533255
dgallard <31977009+dgallard@users.noreply.github.com>
2018-03-09 23:03:11 -1000

    Merge remote-tracking branch 'origin/master' into daniel
    # Conflicts:
    #	.idea/workspace.xml

--- usszhum/app/src/main/java/ncc4702/hpu/edu/usszhum/Obstacle.java
+++ usszhum/app/src/main/java/ncc4702/hpu/edu/usszhum/Obstacle.java
@@ -5,17 +5,20 @@ package ncc4702.hpu.edu.usszhum;
  */
 
 public class Obstacle extends GameElement {
-    private int missPenalty; //the miss penalty for this Obstacle
+    private int hitPenalty; //the miss penalty for this Obstacle
 
     //Constructor
-    public Obstacle(GameView view, int color, int missPenalty, int x, int y, int width, int length, float velocityY){
-        super(view, color, GameView.BLOCKER_SOUND_ID, x, y, width, length, velocityY);
-        this.missPenalty = missPenalty;
+    public Obstacle(GameView view, int color, int hitPenalty, int y, int width){
+        super(view, color, hitPenalty, y, width);
+        this.hitPenalty = hitPenalty;
     }
 
     //returns the miss penalty for this Obstacle
-    public int getMissPenalty(){
-        return missPenalty;
+    public int getHitPenalty(){
+        return hitPenalty;
     }
 
+    public void setHitPenalty(int hitPenalty) {
+        this.hitPenalty = hitPenalty;
+    }
 }
--- usszhum/app/src/main/java/ncc4702/hpu/edu/usszhum/Trash.java
+++ usszhum/app/src/main/java/ncc4702/hpu/edu/usszhum/Trash.java
@@ -4,8 +4,9 @@ package ncc4702.hpu.edu.usszhum;
  * Created by Daniel Gallardo on 3/9/2018.
  */
 
-public class Trash extends Obstacle {
-    public Trash(GameView view, int color, int missPenalty, int x, int y, int width, int length, float velocityY) {
-        super(view, color, missPenalty, x, y, width, length, velocityY);
+public class Trash extends Obstacle {//THIS OBSTACLE IS 1x2
+
+    public Trash(GameView view, int color, int hitPenalty, int y, int width) {
+        super(view, color, hitPenalty, y, width);
     }
 }
