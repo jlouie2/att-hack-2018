189206ed178c00c9db1ae08447fb261cfcdd7c1c
Rhea'da Unpingco <runpingc@my.hpu.edu>
2018-03-09 17:49:52 -1000

    Created Android Studio Project

---
+++ usszhum/app/src/main/java/ncc4702/hpu/edu/usszhum/MainActivity.java
@@ -0,0 +1,13 @@
+package ncc4702.hpu.edu.usszhum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
