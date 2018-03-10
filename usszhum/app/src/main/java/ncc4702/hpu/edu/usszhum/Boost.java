c6aad3a43cb577687f221ca3d605171ae0084c41
dgallard <31977009+dgallard@users.noreply.github.com>
2018-03-09 23:56:18 -1000

    Did stuff. Game element update() method needs work done

--- usszhum/app/src/main/java/ncc4702/hpu/edu/usszhum/Boost.java
+++ usszhum/app/src/main/java/ncc4702/hpu/edu/usszhum/Boost.java
@@ -5,7 +5,8 @@ package ncc4702.hpu.edu.usszhum;
  */
 
 public class Boost extends Obstacle {
-    public Boost(GameView view, int color, int missPenalty, int x, int y, int width, int length, float velocityY) {
-        super(view, color, missPenalty, x, y, width, length, velocityY);
+
+    public Boost(GameView view, int color, int hitPenalty, int y, int width) {
+        super(view, color, hitPenalty, y, width);
     }
 }
